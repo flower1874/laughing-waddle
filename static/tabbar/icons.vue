bd4c0698803e5801fae9b5d1aeaa9bc8ce2f93b7
flower1874 <769949178@qq.com>
2025-04-20 00:15:09 +0800

    🎉 初始化部署目录，提交 build 后的产物

---
+++ static/tabbar/icons.vue
@@ -0,0 +1,19 @@
+<!-- 首页图标 -->
+<svg width="81" height="81" viewBox="0 0 81 81" fill="none">
+  <path d="M40.5 15L15 35.25V66H33.75V45H47.25V66H66V35.25L40.5 15Z" fill="currentColor"/>
+</svg>
+
+<!-- 养护图标 -->
+<svg width="81" height="81" viewBox="0 0 81 81" fill="none">
+  <path d="M60 21H21V60H60V21ZM54 48H27V42H54V48ZM54 39H27V33H54V39Z" fill="currentColor"/>
+</svg>
+
+<!-- 社区图标 -->
+<svg width="81" height="81" viewBox="0 0 81 81" fill="none">
+  <path d="M40.5 15C26.4 15 15 26.4 15 40.5C15 54.6 26.4 66 40.5 66C54.6 66 66 54.6 66 40.5C66 26.4 54.6 15 40.5 15ZM48 43.5H33V37.5H48V43.5Z" fill="currentColor"/>
+</svg>
+
+<!-- 我的图标 -->
+<svg width="81" height="81" viewBox="0 0 81 81" fill="none">
+  <path d="M40.5 40.5C46.8 40.5 51.75 35.55 51.75 29.25C51.75 22.95 46.8 18 40.5 18C34.2 18 29.25 22.95 29.25 29.25C29.25 35.55 34.2 40.5 40.5 40.5ZM40.5 45.75C32.25 45.75 15.75 49.95 15.75 58.125V63H65.25V58.125C65.25 49.95 48.75 45.75 40.5 45.75Z" fill="currentColor"/>
+</svg> 
